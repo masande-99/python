66ec9d08f3acce655f9ba00063663cee7c7a61e2
masande-99 <gontyelenimasande@gmail.com>
2021-06-09 13:57:31 +0200

    First commit

---
+++ area_calculator.py
@@ -0,0 +1,7 @@
+width = int(input("Please enter the width :"))
+length = int(input("Please enter the length :"))
+
+rect_area = width * length
+message = "The area of the rectangle is : "
+
+print(message + str(rect_area))
